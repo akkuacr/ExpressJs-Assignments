1acc61f8db114e13c4f6dd088775d10f9089149d
Akash Chand <111640882+akkuacr@users.noreply.github.com>
2023-09-28 08:43:35 +0530

    Update Question _1.js

--- Question _1.js
+++ Question _1.js
@@ -1,1 +1,82 @@
 
+const http=require('http');
+const fs=require('fs');
+const { hostname } = require('os');
+const port=3000;
+
+const server=http.createServer((request,response)=>{
+    
+   if(request.url=='/')
+   {
+       response.end('<h1>Welcome to Men & Women Dummy Data</h1>')
+   }else if(request.url=='/men')
+   {
+        
+
+      var res=fs.readFileSync("data.json");
+      
+      console.log(res.toString());
+      console.log("Other Stuff First");
+      response.statusCode=500;
+        response.setHeader('Content-Type','application/json');
+
+         response.end(res);
+    
+   }else if(request.url=='/women'){
+      var res=fs.readFileSync("data1.json");
+      
+      console.log(res.toString());
+      console.log("Other Stuff First");
+      response.statusCode=500;
+        response.setHeader('Content-Type','application/json');
+
+         response.end(res);
+
+   }else{
+      response.end('no server found');
+   }
+
+})
+
+
+server.listen(port,()=>{
+   console.log(`Server is listening at ${hostname}:${port}`);
+})
+
+
+//data .json file contains
+[{
+    "id":"1",
+    "name":"men's t-shirts",
+    "brand":"abc"
+ },
+ {
+    "id":"3",
+    "name":"men's t-shirts",
+    "brand":"abc"
+ },
+ {
+    "id":"4",
+    "name":"men's t-shirts",
+    "brand":"abc"
+ }]
+
+//data1.json contains
+[{
+    "id":"1",
+    "name":"women t-shirts",
+    "brand":"abc"
+ },
+ {
+    "id":"3",
+    "name":"Women t-shirts",
+    "brand":"abc"
+ },
+ {
+    "id":"4",
+    "name":"Women t-shirts",
+    "brand":"abc"
+ }]
+
+
+
